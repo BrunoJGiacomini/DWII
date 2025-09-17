1585b26dddf17d2f4f60dbbef5175a75861f5784
BrunoJGiacomini <brunojunigiacomini@gmail.com>
2025-09-17 12:31:19 -0300

    Aula - 01

---
+++ action-cadastro.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dados</title>
+</head>
+<body>
+    <?php
+        $nome = $_GET['txtNome'];
+        $idade = $_GET['txtIdade'];
+        $sexo = $_GET['sexo'];
+
+        echo("Bem-Vindo $nome<br> Idade: $idade anos <br>Sexo: $sexo")
+
+    ?>
+
+    <p>
+        <a href="formulario.php"><button type="button">Voltar</button></a>
+    </p>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+    echo("Vai Corinthians!");
+    $nome = "Brunão";
+    $idade = 21;
+    echo("<br>");
+    echo("Bem vindo $nome e tenho $idade");
+    echo("<br>Bem vindo ".$nome);
+    echo("<br>Idade ".$idade);
+    echo("<br>Bem vindo ".$nome." sua idade ".$idade);
+    echo("<br>");
+    echo("<br>");
+    echo("<p>");
+    echo("<img src='images/sla.png'>");
+    echo("</p>");
+    echo("<a href='formulario.php'>");
+    echo("Formulário");
+    echo("</a>");
+?>
